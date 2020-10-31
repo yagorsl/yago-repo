88ac1eaedeffcfcebb302d7a282e0bbd110173d9
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-10-31 19:25:48 -0300

    resolve exercicio 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_3.js
@@ -0,0 +1,11 @@
+let a = 200;
+let b = 150;
+let c = 70;
+
+if (a > b && a > c){
+    console.log('O maior número é ' + a);
+}else if (b > a && b > c){
+    console.log('O maior número é ' + b);
+}else{
+    console.log('O maior número é ' + c);
+}
