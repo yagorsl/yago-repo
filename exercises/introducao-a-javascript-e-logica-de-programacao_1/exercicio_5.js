93e888126f7c3b5565b970445e1386bc621ff1b0
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-10-31 19:44:05 -0300

    resolve exercicio 5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_5.js
@@ -0,0 +1,10 @@
+let angA = 50;
+let angB = 50;
+let angC = 85;
+
+if ((angA + angB + angC) == 180){
+    console.log(true);
+}else{
+    console.log(false);
+    console.log('Ãngulos inválidos');
+}
