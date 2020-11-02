4aa4b26c00d21e51b9f1997da03a2c55fcb68ae2
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-02 14:02:01 -0300

    resolve exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_4.js
@@ -6,3 +6,8 @@ for (let index = 0; index < numbers.length; index += 1){
 let media = (soma/numbers.length);
 console.log(media);
 
+if (media > 20){
+    console.log('valor maior que 20');
+}else {
+    console.log('valor menor ou igual a 20');
+}
