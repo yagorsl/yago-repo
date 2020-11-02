4ad6fb1f3920a3a2ff466d9be797666b180d8e2f
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2020-11-02 14:47:03 -0300

    refatora codigo exercicio 11

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_11.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_11.js
@@ -1,4 +1,4 @@
-let salarioBruto = 5000;
+let salarioBruto = 3000;
 let salarioBase;
 let aliqInss8 = salarioBruto * 0.08;
 let aliqInss9 = salarioBruto * 0.09;
@@ -8,10 +8,10 @@ let aliqInssMax = salarioBruto - 570.88;
 if (salarioBruto >= 0 && salarioBruto <= 1556.94){
     salarioBase = salarioBruto - aliqInss8;    
 }
-else if (salarioBruto > 1556.94 && salarioBruto <= 2594.92){
+else if (salarioBruto <= 2594.92){
     salarioBase = salarioBruto - aliqInss9;   
 }
-else if (salarioBruto > 2594.92 && salarioBruto <= 5189.82){
+else if (salarioBruto <= 5189.82){
     salarioBase = salarioBruto - aliqInss11;     
 }
 else { 
@@ -28,13 +28,13 @@ let aliqIr4 = (salarioBase * 0.275) - 869.36;
 if (salarioBase >=0 && salarioBase <= 1903.98){
     salarioLiquido = aliqIr0;
     console.log(salarioLiquido + ' ' + 'Isento de IR');
-}else if (salarioBase > 1903.98 && salarioBase <= 2826.65){
+}else if (salarioBase <= 2826.65){
     salarioLiquido = salarioBase - aliqIr1;
     console.log(salarioLiquido);
-}else if (salarioBase > 2826.65 && salarioBase <= 3751.05){
+}else if (salarioBase <= 3751.05){
     salarioLiquido = salarioBase - aliqIr2;
     console.log(salarioLiquido);
-}else if (salarioBase > 3751.05 && salarioBase <= 4664.68){
+}else if (salarioBase <= 4664.68){
     salarioLiquido = salarioBase - aliqIr3;
     console.log(salarioLiquido);
 }else{
